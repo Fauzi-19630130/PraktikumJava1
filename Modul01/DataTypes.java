ac8fc4732b1a6872b440533cb0d245c3a7f07511
Fauzi-19631030 <afuwwu4447@gmail.com>
2021-06-06 21:36:19 +0700

    Hello World

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -1,8 +1,6 @@
 public class DataTypes {
     public static void main(String [] args){
-        String namaDepan ="Mirza";
-        String namaTengah ="Yogy";
-        String namaBelakang ="Kurniawan";
+        String namaDepan ="Fauzi";
         int usia = 18;
         int targetTahunKuliah = 4;
         double ipk = 3.89764512;
