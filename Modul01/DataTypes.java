4b0c00f5fd15e8f9abcb1586c9cac161882f27b2
Fauzi-19631030 <afuwwu4447@gmail.com>
2021-06-06 22:01:28 +0700

    Output dengan MessageDialog

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 public class DataTypes {
     public static void main(String [] args){
         String namaDepan ="Fauzi";
@@ -13,6 +15,8 @@ public class DataTypes {
         System.out.println("IPK : " + ipk);
         System.out.println("Nilai PBO : " + nilaiAbjad);
         System.out.println("Tampan : " + tampan);
+
+        JOptionPane.showMessageDialog(null,"Hai," +namaDepan);
 
     }
 }
