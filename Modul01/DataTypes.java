8aa9d4bbfb87afe469b31a887418dafa26a9a9e4
Fauzi-19631030 <afuwwu4447@gmail.com>
2021-06-06 12:18:37 +0700

    First commit

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,37 @@
+public class DataTypes {
+    public static void main(String [] args){
+        String namaDepan ="Mirza";
+        String namaTengah ="Yogy";
+        String namaBelakang ="Kurniawan";
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.print("Input Nama Depan : ");
+        namaDepan = scanner.nextLine();
+        System.out.print("Input Usia : ");
+        usia = scanner.nextInt();
+        System.out.print("Input IPK : ");
+        ipk = scanner.nextDouble();
+        System.out.print("");
+        System.out.print("Input Nilai Abjad : ");
+        nilaiAbjad = scanner.next().charAt(0);
+        System.out.print("Tampan : ");
+        tampan = scanner.nextBoolean();
+
+        System.out.println("============ OUTPUT ============")
+        System.out.println("Nama depan : " + namaDepan);
+        System.out.println("Nama tengah : " + namaTengah);
+        System.out.println("Nama belakang : " + namaBelakang);
+        System.out.println("Usia : " + usia);
+        System.out.println("Target kuliah : " + targetTahunKuliah  + "tahun");
+        System.out.println("TPK : " + ipk);
+        System.out.println("Nilai PBO : " + nilaiAbjad);
+        System.out.println("Tampan : " + tampan);
+
+        JOptionPane.showMessageDialog(null,"Hai, " + namaDepan + namaTengah + namaBelakang);
+
+    }
+}
