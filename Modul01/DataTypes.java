03073eca1467c4999d7c303d31a0afcdb6ee1346
Fauzi-19631030 <afuwwu4447@gmail.com>
2021-06-06 23:26:10 +0700

    Output dengan sout

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -1,12 +1,18 @@
 public class DataTypes {
     public static void main(String [] args){
         String namaDepan ="Fauzi";
-        
+
         int usia = 21;
         int targetTahunKuliah = 4;
         double ipk = 3.89764512;
         char nilaiAbjad = 'A';
         boolean tampan = true;
 
+        System.out.println("Nama depan : " + namaDepan);
+        System.out.println("Usia : " + usia);
+        System.out.println("Target kuliah : " + targetTahunKuliah  + "tahun");
+        System.out.println("IPK : " + ipk);
+        System.out.println("Nilai PBO : " + nilaiAbjad);
+        System.out.println("Tampan : " + tampan);
     }
 }
