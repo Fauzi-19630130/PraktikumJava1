731fb386cc8ab39943e64750ef04468ac5fd4c4d
Fauzi-19631030 <afuwwu4447@gmail.com>
2021-06-07 21:40:57 +0700

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+    public static void main(String[] args) {
+        
+        String namaDepan = "Fauzi";
+        String namaBelakang = "19630130";
+
+        int usia = 21;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+    }
+}
