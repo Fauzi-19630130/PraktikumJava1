e837028a1a5655475941d49260367414f8c394f3
Fauzi-19631030 <afuwwu4447@gmail.com>
2021-06-08 22:34:15 +0700

    Operasi Tipe String

--- Modul01/Modul01_IDEA/src/Main.java
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String namaDepan = "Fauzi";
@@ -14,8 +16,20 @@ public class Main {
         char[] uniskaChar = {'U','N','I','S','K','A'};
         String uniskaString = new String(uniskaChar);
 
-        System.out.println(uniskaChar);
+        String namaLengkap = namaDepan + " " + npm;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("zi"));
+        System.out.println(namaLengkap.substring(3));
+        System.out.println(namaLengkap.substring(3,10));
+        System.out.println(namaLengkap.replace("Fauzi", "Ambar"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
 
+        String namaArray[] = namaLengkap.split(" ");
+
+        for (String nama : namaArray){
+            System.out.println(nama);
+        }
 
     }
 }
