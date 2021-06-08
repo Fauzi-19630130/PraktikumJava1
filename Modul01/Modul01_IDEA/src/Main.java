9242fccf2022115e723a7c40b6c84610585dfbf6
Fauzi-19631030 <afuwwu4447@gmail.com>
2021-06-08 20:59:16 +0700

    Projek IDEA

---
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Fauzi";
+        String npm = "19630130";
+
+        int usia = 21;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + npm);
+    }
+}
