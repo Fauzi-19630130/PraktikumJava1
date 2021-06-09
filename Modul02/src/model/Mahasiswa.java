c606869d34e9899f5a1607244748ca21c4b3a4db
Fauzi-19631030 <afuwwu4447@gmail.com>
2021-06-09 12:21:26 +0700

    Method Tampilkan Atribut

--- Modul02/src/model/Mahasiswa.java
+++ Modul02/src/model/Mahasiswa.java
@@ -17,4 +17,26 @@ public class Mahasiswa {
     String getNama(){
         return nama;
     }
+
+    void tampilkanAtribut(){
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("06-03-2000");
+        try {
+            this.tanggalLahir = simpleDateFormat.parse("06-03-2000");
+            System.out.println("-----------------------------------------------");
+
+            System.out.println("\t\t ########### " + this.getNama() + " ###########");
+            System.out.println("Nama \t\t\t : " + this.nama);
+            System.out.println("NPM \t\t\t : " + this.npm);
+            System.out.println("Tanggal Lahar \t : " + simpleDateFormat.format(this.tanggalLahir));
+
+            this.menyapa();
+
+            System.out.println("-----------------------------------------------");
+        } catch (ParseException e) {
+            System.err.println("================================================");
+            System.err.println("============= Format Tanggal Salah =============");
+            System.err.println("================================================");
+        }
+    }
 }
--- Modul02/src/model/MahasiswaBeraksi.java
+++ Modul02/src/model/MahasiswaBeraksi.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19630130";
         mahasiswa.nama = "Fauzi";
@@ -13,16 +13,16 @@ public class MahasiswaBeraksi {
 
         String polaTanggal = "dd-MM-yyyy";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("06-03-2000");
-        mahasiswa.tanggalLahir = simpleDateFormat.parse("06-03-2000");
+        try {
+            mahasiswa.tanggalLahir = simpleDateFormat.parse("06-03-2000");
+        } catch (ParseException e) {
+            System.err.println("================================================");
+            System.err.println("============= Format Tanggal Salah =============");
+            System.err.println("================================================");
+        }
 
-        System.out.println("-----------------------------------------------");
+        mahasiswa.tampilkanAtribut();
 
-        System.out.println(mahasiswa.nama);
-        System.out.println(mahasiswa.getNama());
-        System.out.println(simpleDateFormat.format(mahasiswa.tanggalLahir));
 
-        mahasiswa.menyapa();
-
-        System.out.println("-----------------------------------------------");
     }
 }
